e5913802f79408f106b02261b3761e1a7f62f55f
yassine299 <yassinehajjoubi299@gmail.com>
2021-06-29 22:34:20 +0100

    add name field

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -44,6 +44,7 @@ const Channel = ({ user = null, db = null }) => {
             <ul style={{ listStyleType: "none", marginTop: "20px" }}>
                 {messsges.map(message => (
                     <div className="d-flex">
+                        <p style={{width:"30px"}}>{message.displayName}</p>
                         <img style={{ width: "30px", height: "30px", borderRadius: "50px", margin: "5px" }} src={message.photoURL} alt={message.displayName} />
                         <li className="text-break" key={message.id}>{message.text}</li>
                     </div>
