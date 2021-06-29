4eea6e34631791935f88a99c320dc37406299033
yassine299 <yassinehajjoubi299@gmail.com>
2021-06-29 22:13:23 +0100

    ok

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -57,7 +57,7 @@ const Channel = ({ user = null, db = null }) => {
                     onChange={handleonChange}
                     placeholder="Type New Message Her ..."
                 />
-                <button className="btn btn-primary" type="submit" >Send</button>
+                <button className="btn btn-primary" type="submit" >Send it</button>
             </form>
         </div>
     );
